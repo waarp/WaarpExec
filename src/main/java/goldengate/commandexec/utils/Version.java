8a2a4e5227f1885854fc12348a9f9da4b97ee772
Frederic Bregier <nomail@nomail.com>
2012-02-12 19:44:44 +0000

    pom and update dependencies
    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateExec/trunk@1025 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/goldengate/commandexec/utils/Version.java
+++ src/main/java/goldengate/commandexec/utils/Version.java
@@ -3,7 +3,7 @@ package goldengate.commandexec.utils;
 /** Provides the version information of GoldenGate Exec. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "1.0.2";
+ public static final String ID = "1.0.4";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
