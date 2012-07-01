bb3ca7be97bb5b27d96dec4766eadf48bc10c316
Frederic Bregier <nomail@nomail.com>
2012-07-01 23:23:14 +0200

    fix version 1.1.0

--- src/main/java/org/waarp/commandexec/utils/Version.java
+++ src/main/java/org/waarp/commandexec/utils/Version.java
@@ -1,9 +1,9 @@
 // DO NOT MODIFY - WILL BE OVERWRITTEN DURING THE BUILD PROCESS
 package org.waarp.commandexec.utils;
-/** Provides the version information of GoldenGate Exec. */
+/** Provides the version information of Waarp Exec. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "1.0.6";
+ public static final String ID = "1.1.0";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
