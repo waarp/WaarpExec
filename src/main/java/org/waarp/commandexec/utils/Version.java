76615caef40ddc3ede9544341258f088ff0e8c85
Bruno Carlin <bruno.carlin@waarp.org>
2019-07-16 16:56:54 +0200

    version bump

--- src/main/java/org/waarp/commandexec/utils/Version.java
+++ src/main/java/org/waarp/commandexec/utils/Version.java
@@ -3,7 +3,7 @@ package org.waarp.commandexec.utils;
 /** Provides the version information of Waarp Exec. */
 public final class Version {
  /** The version identifier. */
- public static final String ID = "3.0.3";
+ public static final String ID = "3.0.5";
  /** Prints out the version identifier to stdout. */
  public static void main(String[] args) { System.out.println(ID); }
  private Version() { super(); }
